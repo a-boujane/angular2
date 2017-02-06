139b798793831220ad5b11e363d7c3a31371d7ac
a-boujane <a-boujane@i7.com>
2017-02-05 23:55:10 -0500

    Added Angular Material Module to npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule}     from '@angular/forms'
-import {HeroDetailComponent} from './hero-detail.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import {MaterialModule} from '@angular/material'
 
-import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component'
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [
-     BrowserModule,
-     FormsModule
-     ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
+  ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
-     ],
-  bootstrap:    [ AppComponent ]
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
