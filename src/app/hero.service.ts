9c6ed8b752217d741ec5a3af610268cca789e373
a-boujane <a-boujane@i7.com>
2017-02-07 00:59:23 -0500

    Completed the ROuter Module

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,12 +3,21 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero'
 import { HEROES } from './mock-heroes'
 
+
 @Injectable()
 export class HeroService {
+    id:number;
+    constructor(){
+        this.id=Math.random();
+        console.log(this)
+    }
+    
     getHeroes(): Promise<Hero[]> {
+        console.log("called getHeroes in the Service");
         return Promise.resolve(HEROES);
     }
     getHeroesSlowly(): Promise<Hero[]> {
+        console.log("called GetHeroesSlowly");
         return new Promise(resolve => {
             setTimeout(() => resolve(this.getHeroes()), 2000)
         });
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,31 +1,41 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
-import { OnInit } from '@angular/core'
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
-  moduleId:module.id,
+  moduleId: module.id,
   selector: 'my-heroes',
-  templateUrl:'./heroes.component.html',
-  styleUrls:['./heroes.component.css'],
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private router: Router
+  ) { }
 
   title: string = 'Tour of Heros';
   heroes: Hero[];
   selectedHero: Hero;
 
-  ngOnInit():void{
+  ngOnInit(): void {
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().then(heroes=>this.heroes=heroes);
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
 }
